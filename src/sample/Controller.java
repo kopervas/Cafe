325d783a22fd1c176ce4b72100db4025d2cd5035
kopervas <sergkopervas@gmail.com>
2018-06-21 22:07:51 +0300

    Трохи заплутався. Але якось воно працює. Якось...

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -24,21 +24,24 @@ import java.net.URL;
 import java.util.ArrayList;
 import java.util.Random;
 import java.util.ResourceBundle;
+import java.util.concurrent.TimeUnit;
 
 public class Controller implements Initializable {
+    //лічильник спроб. Рахується від 0 до 2
+    private int count;
+    //загальна кількість спроб гравця
+    private int total_attempt = 0;
     //головний лічильник балів, набраних гравцем
     private int total_score = 0;
     //поточний лічильник балів, набраних гравцем
-    private int score = 0;
+    private int score = 10;
     //номер страви, яку обрав клієнт
     private int client_change = 0;
     //номер страви, яку обрав гравець
     private int gamer_change;
-    //загальна кількість спроб гравця
-    private int total_attempt = 0;
     //кількість спроб гравця
     private int count_attempt = 0;
-    //кількість клієнтів, яку обслуговували
+    //кількість клієнтів, яку обслуговували - мабуть, це поле є зайвим
     private int count_clients = 0;
     //індекс клієнта, який генерується методом setClient()
     private int number_client;
@@ -86,29 +89,30 @@ public class Controller implements Initializable {
     @FXML
     public void initialize(URL location, ResourceBundle resources) {
         Random random = new Random();
-        number_client = random.nextInt(6);
+        setClient();                                                //вибір першого клієнта при завантаженні програми
 
-        fillFoods();
+        fillFoods();                                                //заповнення списку продуків
         first_order.setGraphic(new ImageView(foods.get(0)));
         two_order.setGraphic(new ImageView(foods.get(1)));
         three_order.setGraphic(new ImageView(foods.get(2)));
 
-        fillClients();
-        ClientChangeFood(random.nextInt(2));
-        setClient();
-        setClientFoto(number_client, 1);
+        fillClients();                                              //заповнення списку клієнтів
+        ClientChangeFood(random.nextInt(3));                 //вибір першої страви першим клієнтом
+
+        setClientFoto(number_client, 1);                         //виведення у поле client_foto зображення клієнта
 
 
         first_order.setOnAction(new javafx.event.EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
-                count_attempt += 1;
-                gamer_change = 0;
-                change_cafe.setImage(foods.get(0));
-                Game();
-                change_client.setImage(foods.get(client_change));
-                ClientChangeFood(random.nextInt(2));
-                label.setText(String.valueOf(count_attempt));
+                //total_attempt += 1;                                 //збільшується лічильник спроб гравця (на кожного клієнта - 3 спроби)
+                count++;
+                gamer_change = 0;                                   //номер страви, яку обрав гравець, натиснувши на дану кнопку (потрібно для порівнянн з вибором клієнта)
+                change_cafe.setImage(foods.get(0));                 //вивід зображення страви, яку обрав гравець, натиснувши дану кнопку
+                Game();                                             //основний метод гри - перевірка кількості набраних гравцем балів і відповідна дія
+                change_client.setImage(foods.get(client_change));   //після того, як гравець зробив свій хід, показується страва, обрана клієнтом
+                ClientChangeFood(random.nextInt(3));         //клієнт тут же "обирає" нову страву
+                label.setText(String.valueOf(count));       //індикатор спроб показує, скільки спроб зроблено
                 System.out.println(client_change);
             }
 
@@ -117,13 +121,14 @@ public class Controller implements Initializable {
         two_order.setOnAction(new javafx.event.EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
-                count_attempt += 1;
+                //total_attempt += 1;
+                count++;
                 gamer_change = 1;
                 change_cafe.setImage(foods.get(1));
                 Game();
                 change_client.setImage(foods.get(client_change));
-                ClientChangeFood(random.nextInt(2));
-                label.setText(String.valueOf(count_attempt));
+                ClientChangeFood(random.nextInt(3));
+                label.setText(String.valueOf(count));
                 System.out.println(client_change);
             }
         });
@@ -131,21 +136,19 @@ public class Controller implements Initializable {
         three_order.setOnAction(new javafx.event.EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
-                count_attempt += 1;
+                //total_attempt += 1;
+                count++;
                 gamer_change = 2;
                 change_cafe.setImage(foods.get(2));
                 Game();
                 change_client.setImage(foods.get(client_change));
-                ClientChangeFood(random.nextInt(2));
-                label.setText(String.valueOf(count_attempt));
+                ClientChangeFood(random.nextInt(3) );
+                label.setText(String.valueOf(count));
                 System.out.println(client_change);
             }
         });
 
         results.setText("Спроб - 0, балів - 0");
-        //client_foto.setImage(new Boy().getEmotion());//(new ImageView("./resources/CafeNew/Boy/Boy-01.png")));
-        client_foto.getImage();
-        //setClientFoto(10);
     }
 
     /**
@@ -154,65 +157,96 @@ public class Controller implements Initializable {
      */
     public void Game(){
         Random random = new Random();
-        if(client_change == gamer_change)
+        //int count = 0;
+
+        if(client_change == gamer_change)   //збілшення балів, коли гравець вгадав, яку страву обрав клієнт
         {
             score +=10;
+            if(score >= 20)
+                setClientFoto(number_client, 0);
+            else if(score <= 0 && score > -10)
+                setClientFoto(number_client, 2);
+            else if(score < -10)
+                setClientFoto(number_client, 3);
         }
-        else{
-            score -= 5;
+        else{                               //зменшення балів, якщо гравець не вгадав
+            score -= 10;
+            if(score >= 20)
+                setClientFoto(number_client, 0);
+            else if(score <= 0 && score > -10)
+                setClientFoto(number_client, 2);
+            else if(score < -10)
+                setClientFoto(number_client, 3);
         }
+        checkScore();
+    }
 
-        if (score >= 0 && score < 200 ){
-            ClientChangeFood(random.nextInt(2));
-        }
-        else if(score < 0){
-            try{
-                Stage stage = new Stage();
-                Parent root = FXMLLoader.load(getClass().getResource("../resources/wrong.fxml"));
-                stage.setTitle("Ви програли!");
-                stage.setMinHeight(150);
-                stage.setMinWidth(300);
-                stage.setResizable(false);
-                stage.setScene(new Scene(root));
-                stage.initModality(Modality.WINDOW_MODAL);
-                stage.initOwner(pane.getScene().getWindow());
-                stage.show();
-            }catch (IOException e){
+    /**
+     * Метод перевірки рівня
+     */
+    private void checkScore(){
+        Random random = new Random();
+        if (score > -200 && score < 100 ){
+            if(countClient() == true) {
+                //ClientChangeFood(random.nextInt(3));
+                results.setText("Кількість спроб " + total_attempt + ". Всього балів " + total_score);
+            }
+            else{
+                setClient();
+            }
+            try {
+                TimeUnit.SECONDS.sleep(1);
+            } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-            client_foto.setImage(clients.get(number_client).getEmotion(3));
+            total_attempt += 1;
+        }
+        else if(score < -200){                                                                  //точка виходу - борг стає більше 200 балів
+            client_foto.setImage(clients.get(number_client).getEmotion(3));                         //встановлення іконки емоції клієнта
+            try {
+                TimeUnit.SECONDS.sleep(10);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
             setClient();
             setClientFoto(number_client, 1);
-            ClientChangeFood(random.nextInt(2));
+            ClientChangeFood(random.nextInt(3));
             results.setText("Кількість спроб " + total_attempt + ". Всього балів " + total_score);
             total_attempt += 1;
             total_score += score;
             score = 0;
         }
-        else if(score > 200){
-            try{
-                Stage stage = new Stage();
-                Parent root = FXMLLoader.load(getClass().getResource("../resources/wins.fxml"));
-                stage.setTitle("Ви виграли!");
-                stage.setMinHeight(150);
-                stage.setMinWidth(300);
-                stage.setResizable(false);
-                stage.setScene(new Scene(root));
-                stage.initModality(Modality.WINDOW_MODAL);
-                stage.initOwner(pane.getScene().getWindow());
-                stage.show();
-            }catch (IOException e){
-                e.printStackTrace();
-            }
+        else if(score > 100){                                                                   //точка виходу - виграш стає більше 100 балів
             client_foto.setImage(clients.get(number_client).getEmotion(0));
             setClient();
             setClientFoto(number_client, 1);
-            ClientChangeFood(random.nextInt(2));
+            ClientChangeFood(random.nextInt(3));
             results.setText("Кількість спроб " + total_attempt + ". Всього балів " + total_score);
             total_attempt += 1;
             total_score += score;
             score = 0;
         }
+    }
+
+    /**
+     * Метод перевіряє кількість спроб на кожного клієнта - поле count.
+     * На кожного клієнта дається 3 спроби
+     */
+    private boolean countClient(){
+        if(count < 3) {
+            count++;
+            return true;
+        }
+        else {
+            count = 0;
+            setClient();
+            count_clients++;
+            total_score += score;
+            total_attempt += 1;
+            score = 10;
+            System.out.println("Цикл № "+count_attempt/4 +" закінчився ");
+        }
+        return false;
     }
 
     /**
@@ -258,7 +292,7 @@ public class Controller implements Initializable {
      */
     private void setClient(){
         Random rand = new Random();
-        number_client = rand.nextInt(6);
+        number_client = rand.nextInt(6) - 1;
     }
 
     /**
